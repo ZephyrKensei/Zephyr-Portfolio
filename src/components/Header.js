9912524d07c78d212af0c3a42cd59b712dbbb868
Zephyr <82427116+ZephyrKensei@users.noreply.github.com>
2023-07-07 13:42:35 -0500

    working props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 
 function Header() {
 
-  const navToggle = document.querySelector('.nav-toggle');
-  const navLinks = document.querySelectorAll('.nav__link')
+//   const navToggle = document.querySelector('.nav-toggle');
+//   const navLinks = document.querySelectorAll('.nav__link')
 
-  navToggle.addEventListener('click', () => {
-      document.body.classList.toggle('nav-open');
-  });
+//   navToggle.addEventListener('click', () => {
+//       document.body.classList.toggle('nav-open');
+//   });
 
-  navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-          document.body.classList.remove('nav-open');
-      })
-  })
+//   navLinks.forEach(link => {
+//       link.addEventListener('click', () => {
+//           document.body.classList.remove('nav-open');
+//       })
+//   })
 
   return (
     <header>
