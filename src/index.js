10dc13b773ea332d32530a12a1d86f80e4f25dff
Zephyr <82427116+ZephyrKensei@users.noreply.github.com>
2023-08-06 14:00:54 -0500

    changing css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import './css/App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
